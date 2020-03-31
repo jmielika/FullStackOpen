49c32939eba64434eb656e4b77d05f61aa1848d6
Joonas Mielikäinen <joonas.mielikainen@gmail.com>
2020-03-31 21:03:46 +0300

    Luotu React sovellus osa 1, tehtävät 1.1 ja 1.2

---
+++ osa-1/kurssitiedot/src/index.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      
+      <Part part={props.part1} excercise={props.exercises1} />
+      <Part part={props.part2} excercise={props.exercises2} />
+      <Part part={props.part3} excercise={props.exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of excercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}
+      </p>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.excercise}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const parts = [part1, part2, part3]
+  const exercises = [exercises1, exercises2, exercises3]
+
+  return (
+    <>
+      <Header course={course} />
+      <Content 
+      part1={part1} exercises1={exercises1}
+      part2={part2} exercises2={exercises2}
+      part3={part3} exercises3={exercises3} />
+      <Total exercises={exercises} />
+    </>
+
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
