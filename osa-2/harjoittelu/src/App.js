e622b7126468f4799119dbdb1dadc94bbd7b28f9
Joonas Mielikäinen <joonas.mielikainen@gmail.com>
2020-06-10 14:48:34 +0300

    tehty harjoitustehtävät ennen 2.6 kohdan tehtäviä. luotu lista yleisimmistä komennoista. luotu puhelinluettelon pohja, ei vielä kirjoitettu mitään koodia

--- osa-2/harjoittelu/src/App.js
+++ osa-2/harjoittelu/src/App.js
@@ -1,15 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewnote] = useState(
+    'a new note...'
+  )
+  const [showAll, setShowAll] = useState(true)
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  const addNote = (event) => {
+      event.preventDefault()
+      const noteObject = {
+        content: newNote,
+        date: new Date().toISOString(),
+        important: Math.random() > 0.5,
+        id: notes.length + 1
+      }
+
+      setNotes(notes.concat(noteObject))
+      setNewnote('')
+  }
+
+  const handleNoteChange = (event) => {
+    setNewnote(event.target.value)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => 
-          <Note key={note.id} note={note} />
+        {notesToShow.map((note,i) => 
+          <Note key={i} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
