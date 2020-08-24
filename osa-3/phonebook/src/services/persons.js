4c15fb5dbb2b17d212a65f6dc26a55a4891a5e35
Joonas Mielikäinen <joonas.mielikainen@gmail.com>
2020-08-24 17:35:36 +0300

    uibuild

--- osa-3/phonebook/src/services/persons.js
+++ osa-3/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://tranquil-sands-81334.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
